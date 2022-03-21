5a0474f41afce6e48deab0d40d0d9c41a8349fcd
misanthropicat <olga.mogish@dell.com>
2022-03-21 16:19:20 +0300

    configure ingress minikube addon

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ then
         chmod 700 get_helm.sh
         ./get_helm.sh
 fi
-docker build -t localhost:5000/webapp:latest .
+docker build -t olgamogish/postgres-fastapi-helm:latest .
+docker push olgamogish/postgres-fastapi-helm:latest
 helm install postgres postgres
 helm install webapp webapp
+kubectl patch configmap tcp-services -n ingress-nginx --patch "{\"data\":{\"8080\":\"default/webapp:8080\"}}"
+kubectl patch deployment ingress-nginx-controller --patch "$(cat ingress-nginx-controller-patch.yaml)" -n ingress-nginx
